4720937af92ef12fefc899998272a871c3cd16c4
Shizhan Xu <shizhanx@student.unimelb.edu.au>
2021-10-28 12:24:16 +0800

    Implement the skeleton for list room fragment. Currently only shows a title and an empty list.

--- app/src/main/java/com/example/pokersimulator/ListRoomFragment.kt
+++ app/src/main/java/com/example/pokersimulator/ListRoomFragment.kt
@@ -5,21 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.pokersimulator.databinding.ListRoomFragmentBinding
+import com.example.pokersimulator.databinding.RoomFragmentBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class ListRoomFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: ListRoomFragmentBinding? = null
+    private val activityViewModel: MainActivityViewModel by activityViewModels()
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_room_fragment, container, false)
+    ): View {
+        _binding = ListRoomFragmentBinding.inflate(inflater, container, false)
+        binding.textviewListRoomHeader.text = getString(R.string.welcome_username, activityViewModel.username)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO define navigation to the room fragment
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
