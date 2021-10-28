c51ad5fb612b406dc73107377a98649970f57b9c
Shizhan Xu <shizhanx@student.unimelb.edu.au>
2021-10-28 11:32:26 +0800

    Add a new list room fragment skeleton

---
+++ app/src/main/java/com/example/pokersimulator/ListRoomFragment.kt
@@ -0,0 +1,25 @@
+package com.example.pokersimulator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ListRoomFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.list_room_fragment, container, false)
+    }
+}
