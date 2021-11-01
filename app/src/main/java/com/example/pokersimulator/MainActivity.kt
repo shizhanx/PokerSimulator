1c02a16d10b500ae2f6a096ddcf963b4eb0f88eb
Shizhan Xu <shizhanx@student.unimelb.edu.au>
2021-11-01 09:59:53 +0800

    Add a menu for the sorting options

--- app/src/main/java/com/example/pokersimulator/MainActivity.kt
+++ app/src/main/java/com/example/pokersimulator/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.pokersimulator
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
-import android.view.MenuItem
 import android.view.WindowManager
 import androidx.activity.viewModels
 import com.example.pokersimulator.databinding.ActivityMainBinding
@@ -38,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_sorting_options, menu)
         return true
     }
 
